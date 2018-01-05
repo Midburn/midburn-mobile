efc0eda57e55b8375ec68ca7ef2121ef0fe5285a
Ran Greenberg <rang@wix.com>
2018-01-06 00:32:46 +0200

    add navigation to android

--- MidburnMobileApp/android/app/src/main/java/com/midburnmobileapp/MainActivity.java
+++ MidburnMobileApp/android/app/src/main/java/com/midburnmobileapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.midburnmobileapp;
 
-import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+//public class MainActivity extends ReactActivity {
+//
+//    /**
+//     * Returns the name of the main component registered from JavaScript.
+//     * This is used to schedule rendering of the component.
+//     */
+//    @Override
+//    protected String getMainComponentName() {
+//        return "MidburnMobileApp";
+//    }
+//}
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "MidburnMobileApp";
-    }
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+
 }
