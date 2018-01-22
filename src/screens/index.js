b4feb9c0c25431a60bc1da9822ddeadf12baa218
Ran Greenberg <rang@wix.com>
2018-01-22 08:14:41 +0200

    add and register camp screen

--- src/screens/index.js
+++ src/screens/index.js
@@ -3,10 +3,13 @@ import SCREENS from './screenNames';
 import CampsTab from './CampsTab';
 import SecondTab from './ProgramTab';
 import MapTab from './MapTab';
+import CampScreen from './CampsTab/CampScreen'
 
 // register all screens of the app (including internal ones)
 export function registerContainers() {
   Navigation.registerComponent(SCREENS.CAMPS, () => CampsTab);
   Navigation.registerComponent(SCREENS.PROGRAM, () => SecondTab);
   Navigation.registerComponent(SCREENS.MAP, () => MapTab);
+
+  Navigation.registerComponent(SCREENS.CAMP_SCREEN, () => CampScreen);
 }
--- src/screens/screenNames.js
+++ src/screens/screenNames.js
@@ -2,9 +2,14 @@
 const PREFIX = 'midburn';
 
 const SCREENS = {
+
+  // tabs root
   CAMPS: `${PREFIX}.CAMPS`,
   PROGRAM: `${PREFIX}.PROGRAM`,
   MAP: `${PREFIX}.MAP`,
+
+  // screens
+  CAMP_SCREEN: `${PREFIX}.CAMP_SCREEN`,
 };
 
 export default SCREENS;
