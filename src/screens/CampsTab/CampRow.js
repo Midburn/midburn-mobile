5e904b75c3eebe94c7d590a8ead0ec0cf744ae1b
Ran Greenberg <rang@wix.com>
2018-01-22 08:14:53 +0200

    update camp row

--- src/screens/CampsTab/CampRow.js
+++ src/screens/CampsTab/CampRow.js
@@ -4,14 +4,13 @@ import {Text, View, Button} from 'react-native-ui-lib';
 export default class CampsTab extends Component {
 
   _onPress = () => {
-    // console.error('RANG', '_onPress', this.props);
     this.props.onPress(this.props.data);
   }
 
   render() {
     return (
-      <View style={{flex: 1, margin: 5, backgroundColor: 'green'}}>
-        <Button link label={this.props.data.name_en} onPress={this._onPress}/>
+      <View flex center paddingV-10>
+          <Button link linkColor={'black'} label={this.props.data.name_en} onPress={this._onPress}/>
       </View>
     );
   }
