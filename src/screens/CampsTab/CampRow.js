c6e1711bbd78967f48b1542b51d3f76f036959f2
Ran Greenberg <rang@wix.com>
2018-02-10 20:23:10 +0200

    add demo icons to camp row

--- src/screens/CampsTab/CampRow.js
+++ src/screens/CampsTab/CampRow.js
@@ -1,16 +1,51 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
-import {Text, View, Button} from 'react-native-ui-lib';
+import {Image} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native-ui-lib';
+import {getRandomImagesArray}from '../../../data/img';
 
 export default class CampsTab extends Component {
+
+  constructor(props) {
+    super(props);
+    this.icons = getRandomImagesArray();
+  }
+
 
   _onPress = () => {
     this.props.onPress(this.props.data);
   }
 
+  _renderSidePanel() {
+    return (
+      <View>
+        {_.map(this.icons, (src, key) => {
+          return (
+            <Image key={key} source={src} style={{width: 30, height: 30, margin: 4}}/>
+          );
+        })}
+
+      </View>
+    );
+  }
+
+  _renderContent() {
+    return (
+      <View flex center>
+        <Text>{this.props.data.name_en}</Text>
+      </View>
+    );
+  }
+
   render() {
     return (
-      <View flex center paddingV-10>
-        <Button link linkColor={'black'} label={this.props.data.name_en} onPress={this._onPress}/>
+      <View flex bg-dark70 margin-16 marginB-0 paddingV-8 style={{borderRadius: 3}}>
+        <TouchableOpacity onPress={this._onPress} style={{flex: 1}}>
+          <View row left flex>
+            {this._renderSidePanel()}
+            {this._renderContent()}
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
