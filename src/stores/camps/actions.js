50ee6ddebc36321ae02e73f8e7a3381de74856db
Ran Greenberg <rang@wix.com>
2018-03-18 21:15:12 +0200

    extract logic from camp screen to actions

--- src/stores/camps/actions.js
+++ src/stores/camps/actions.js
@@ -1,10 +1,19 @@
 import * as store from './store'
+import SCREEN_NAMES from "../../screens/screenNames";
 
 export function loadCamps() {
   const data = require('../../../data/camps');
   store.setters.setCamps(data.ToPublish)
 }
 
+export function showCampScreen({data, navigator}) {
+  navigator.push({
+    screen: SCREEN_NAMES.CAMP_SCREEN,
+    passProps: {data},
+    title: data.name_en
+  })
+}
+
 export function applySearch(text) {
-  
+  console.log('RANG', 'applySearch', 'TBD');
 }
