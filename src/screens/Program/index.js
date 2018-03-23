4a1138aebf64ea30eab83e46a724323367f01240
Nir Yosef <niryo@wix.com>
2018-03-23 17:10:35 +0300

    better looking program days bar

--- src/screens/Program/index.js
+++ src/screens/Program/index.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
-import {
-  Platform,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView
-} from 'react-native';
+import {Platform, StyleSheet,TouchableOpacity,ScrollView} from 'react-native';
 import {Text, View, Button} from 'react-native-ui-lib';
 import {HoursScheduleComponent} from './HoursScheduleComponent';
 import * as _ from 'lodash';
 
+
+const MIDBURN_STARTING_DATE = 14;
 //fake events, should be removed!
 const events = [{
   name: 'old event',
@@ -17,24 +14,24 @@ const events = [{
 }, {
   name: 'now',
   start: Date.now() + 1 * 60000,
-  end: Date.now() + 30 *60000
+  end: Date.now() + 30 * 60000
 },
 {
   name: 'now2',
   start: Date.now() + 1 * 60000,
-  end: Date.now() + 30 *60000
-},{
+  end: Date.now() + 30 * 60000
+}, {
   name: 'two hours from now',
-  start: Date.now() + 120 *60000,
-  end:  Date.now() + 150 *60000
+  start: Date.now() + 120 * 60000,
+  end: Date.now() + 150 * 60000
 }, {
   name: 'half an hour from now',
-  start: Date.now() + 30 *60000,
-  end: Date.now() + 200 *60000
-},{
+  start: Date.now() + 30 * 60000,
+  end: Date.now() + 200 * 60000
+}, {
   name: '40 min from now',
-  start:  Date.now() + 40 *60000,
-  end:  Date.now() + 330 *60000,
+  start: Date.now() + 40 * 60000,
+  end: Date.now() + 330 * 60000,
 }];
 
 export default class ProgramScreen extends Component {
@@ -66,48 +63,44 @@ export default class ProgramScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{height: 60, backgroundColor: 'gray'}}>
+        <View>
           <ScrollView horizontal showsHorizontalScrollIndicator={false}>
             {
               this.state.dates.map((date, i) => this.renderDateItem(date, i))
             }
           </ScrollView>
         </View>
-        <HoursScheduleComponent events={events} firstHour={0} lastHour={24} highlightCurrentTime={false}/>
+        <HoursScheduleComponent events={events} firstHour={0} lastHour={24} highlightCurrentTime={false} />
       </View>
     );
   }
 
   renderDateItem(date, i) {
-
     return (
-      <TouchableOpacity onPress={() => this.dateItemPressed(date, i)} key={date} style={{height: 60, width: 100, backgroundColor: this.state.selectedDateIndex === i ? 'gray' : 'white', alignItems: 'center', justifyContent: 'center'}}>
+      <TouchableOpacity onPress={() => this.dateItemPressed(date, i)} key={date} style={[styles.dateItem, this.state.selectedDateIndex === i && styles.dateItemSelected]}>
         <Text>{date}</Text>
+        <Text text100>{i + MIDBURN_STARTING_DATE}</Text>
       </TouchableOpacity>
     );
   }
 
-  renderEvent(event, i) {
-    return (
-      <View key={event.name + i} style={{backgroundColor: 'white', height: 100, margin: 10, padding: 10}}>
-        <Text text60>{event.name}</Text>
-        <Text text80>{event.description}</Text>
-      </View>
-    );
-  }
-
   dateItemPressed(date, i) {
-    this.setState(_.merge(this.state, {selectedDateIndex: i}));
+    this.setState(_.merge(this.state, { selectedDateIndex: i }));
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'gray',
+    flex: 1
   },
-  scrollView: {
-    backgroundColor: 'gray',
-    height: 50
+  dateItem: {
+    height: 35,
+    width: 75,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  dateItemSelected: {
+    backgroundColor: 'gray'
   }
 });
