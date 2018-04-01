49a6d7ad6dd743a3ec8fa410c8148a3ab5628e8a
Ran Greenberg <rang@wix.com>
2018-04-01 20:31:19 +0300

    add art tab

---
+++ src/screens/Art/ArtRow.js
@@ -0,0 +1,61 @@
+import _ from 'lodash';
+import React, {PureComponent} from 'react';
+import {Image} from 'react-native';
+import {Text, View, Card} from 'react-native-ui-lib';
+import {tagToImg} from '../../../data/img';
+
+const AVARTAR_SIZE = 30;
+const AVATAR_MARGIN = 4;
+const AVATAR_CONTAINER_WIDTH = AVARTAR_SIZE*2 + AVATAR_MARGIN*4;
+
+export default class ArtRow extends PureComponent {
+
+  constructor(props) {
+    super(props);
+  }
+
+
+  _onPress = () => {
+    this.props.onPress(this.props.data);
+  }
+
+
+  _renderSidePanel() {
+    return (
+      <View style={{width: AVATAR_CONTAINER_WIDTH, flexWrap: 'wrap', alignContent: 'flex-end'}} row spread>
+        {_.map(this.props.tags, (src, key) => {
+          return (
+            <Image
+              key={key}
+              source={tagToImg(src)}
+              style={{width: AVARTAR_SIZE, height: AVARTAR_SIZE, margin: AVATAR_MARGIN}}
+            />
+          );
+        })}
+      </View>
+
+    );
+  }
+
+  _renderContent() {
+    return (
+      <View flex row>
+
+        <Text text60 margin-19>{this.props.title}</Text>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <Card onPress={this._onPress} key={this.props.index} containerStyle={{marginBottom: 15}}>
+        <Card.Image height={120} imageSource={} />
+        <Card.Section style={{flexDirection: 'row', alignItems: 'center'}} body>
+
+          <Text text50>{this.props.title}</Text>
+        </Card.Section>
+
+      </Card>
+    );
+  }
+}
--- src/screens/Art/index.js
+++ src/screens/Art/index.js
@@ -1,39 +1,142 @@
 import React, {Component} from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import {FlatList} from 'react-native';
+import {View, TabBar, TextInput, Text} from 'react-native-ui-lib';
+import {connect} from 'remx';
+import ArtRow from './ArtRow';
+import IndicatorBar from '../components/IndicatorBar';
+import * as store from '../../stores/campsAndArt/store';
+import * as actions from './../../stores/campsAndArt/actions';
+import {getRandomImage} from '../../../data/img';
+
+const SEARCH_BUTTON_ID = 'camp_search';
+const PLACEHOLDER_SEARCH_INPUT = 'Type camp to serach';
+
+const FILTER = {
+  CAMPS: 'Camps',
+  ART: 'Art'
+};
 
 
-export default class ArtScreen extends Component {
+class ArtScreen extends Component {
+
+  static navigatorButtons = {
+    rightButtons: [
+      {
+        id: 'camp_search',
+        systemItem: 'search'
+      }
+    ]
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSearchBar: false
+    };
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+  }
+
+  onNavigatorEvent(event) {
+    if (event.id == SEARCH_BUTTON_ID) {
+
+      this.setState({showSearchBar: !this.state.showSearchBar});
+      if (this.searchTextInputRef) {
+        this.searchTextInputRef.focus();
+      }
+    }
+  }
+
+
+  _onRowPressed = async (data) => {
+    actions.showCampScreen({data, navigator: this.props.navigator});
+  }
+
+  _onFavouriteFilterBarPressed = () => {
+    this._onFilterBarPressed(FILTER.ART);
+
+  }
+
+  _renderRow = (data) => {
+    return (
+      <ArtRow
+        data={data.item}
+        campId={data.item.campId}
+        title={data.item.title}
+        tags={data.item.tags}
+        imageUrl={data.item.imageUrl}
+        onPress={this._onRowPressed}
+      />
+    );
+  }
+
+  onTextChanged = (text) => {
+    store.setters.setSearch(text)
+  }
+
+  onTagsFilterPressed = (index) => {
+    store.setters.setSelectedTagIndex(index);
+
+  }
+
+  _renderSearchBar() {
+    return (
+      <View paddingH-8>
+        <TextInput
+          ref={(ref) => this.searchTextInputRef = ref}
+          autoCorrect={false}
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={this.onTextChanged}
+          value={this.state.text}
+          placeholder={PLACEHOLDER_SEARCH_INPUT}
+        />
+      </View>
+    );
+  }
+
+  renderArtList() {
+    return (
+      <FlatList
+        key={'art'}
+        style={{padding: 15, backgroundColor: '#fab1a0' }}
+        data={this.props.artData}
+        renderItem={this._renderRow}
+        keyExtractor={(item, index) => index}
+        initialNumToRender={10}
+      />
+    );
+  }
+
+  _renderTagsFilterBar() {
+    return (
+      <IndicatorBar
+        itemSize={40}
+        items={this.props.tags}
+        containerStyle={{padding: 8}}
+        selectedIndex={this.props.selectedTagIndex}
+        onChangeIndex={this.onTagsFilterPressed}
+      />
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          ART TAB
-        </Text>
+      <View flex>
+        {this.state.showSearchBar && this._renderSearchBar()}
+        {this.renderArtList()}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'pink',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+function mapStateToProps() {
+  return {
+    campsData: store.getters.getCampsDataToShow(),
+    artData: store.getters.getArtDataToShow(),
+    searchText: store.getters.getSearchText(),
+    selectedTab: store.getters.getSelectedTab(),
+    selectedTagIndex: store.getters.getSelectedTagIndex(),
+    tags: store.getters.getAllTags()
+  };
+}
+
+export default connect(mapStateToProps)(ArtScreen);
