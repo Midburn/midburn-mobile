d57b949aff58bc1144e7578c1d6fb56a2e264ab7
Ran Greenberg <rang@wix.com>
2018-04-08 11:42:55 +0300

    add stub to tags in camps row

--- src/screens/Camps/CampRow.js
+++ src/screens/Camps/CampRow.js
@@ -2,16 +2,22 @@ import _ from 'lodash';
 import React, {PureComponent} from 'react';
 import {Image} from 'react-native';
 import {Text, View, Card} from 'react-native-ui-lib';
-import {tagToImg} from '../../../data/img';
+import {tagToImg, getRandomImage} from '../../../data/img';
 
+const CAMP_PLACEHOLDER = require('../../../data/img/camp-placeholder.png');
 const AVARTAR_SIZE = 30;
 const AVATAR_MARGIN = 4;
 const AVATAR_CONTAINER_WIDTH = AVARTAR_SIZE*2 + AVATAR_MARGIN*4;
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export default class CampsTab extends PureComponent {
 
   constructor(props) {
     super(props);
+    this.tags = this.props.tags || _.times(getRandomInt(1, 6), getRandomImage);
   }
 
 
@@ -23,11 +29,11 @@ export default class CampsTab extends PureComponent {
   _renderSidePanel() {
     return (
       <View style={{width: AVATAR_CONTAINER_WIDTH, flexWrap: 'wrap', alignContent: 'flex-end'}} row spread>
-        {_.map(this.props.tags, (src, key) => {
+        {_.map(this.tags, (src, key) => {
           return (
             <Image
               key={key}
-              source={tagToImg(src)}
+              source={src}
               style={{width: AVARTAR_SIZE, height: AVARTAR_SIZE, margin: AVATAR_MARGIN}}
             />
           );
