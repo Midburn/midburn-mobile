ba4d27aeed60f3be0f39b71fe9fa6a47309e1571
Ran Greenberg <rang@wix.com>
2018-04-18 10:21:47 +0300

    update safety screen

--- src/screens/Extra/SafetyScreen.js
+++ src/screens/Extra/SafetyScreen.js
@@ -5,20 +5,7 @@ import { Button, Text, View } from 'react-native-ui-lib';
 import {isRTL} from './../../utils/Strings';
 
 
-const principlesData = require('../../../data/2018/info/principles.json').principles;
-
-const iconMap = {
-  'radicalinclusion.png': require('../../../data/2018/images/info/radicalinclusion.png'),
-  'communaleffort.png': require('../../../data/2018/images/info/communaleffort.png'),
-  'gifting.png': require('../../../data/2018/images/info/gifting.png'),
-  'civicresponsability.png': require('../../../data/2018/images/info/civicresponsability.png'),
-  'decommodification.png': require('../../../data/2018/images/info/decommodification.png'),
-  'leavingnotrace.png': require('../../../data/2018/images/info/leavingnotrace.png'),
-  'radicalselfreliance.png': require('../../../data/2018/images/info/radicalselfreliance.png'),
-  'participation.png': require('../../../data/2018/images/info/participation.png'),
-  'radicalselfexpression.png': require('../../../data/2018/images/info/radicalselfexpression.png'),
-  'immediacy.png': require('../../../data/2018/images/info/immediacy.png')
-};
+const safetyData = require('../../../data/2018/info/safety.json');
 
 export default class SafetyScreen extends Component {
 
@@ -27,19 +14,13 @@ export default class SafetyScreen extends Component {
       <View flex>
         <ScrollView>
           {
-            _.map(principlesData, (principle) => {
+            _.map(safetyData, (obj) => {
               return (
-                <View padding-12 key={principle.name} right={isRTL()}>
+                <View padding-12 key={obj.name} right={isRTL()}>
                   <View center={isRTL()} centerH={!isRTL()} style={{flexDirection: isRTL() ? 'row-reverse' : 'row'}}>
-                    <View paddingR-8={!isRTL()} paddingL-8={isRTL()}>
-                      <Image
-                        source={iconMap[principle.logo]}
-                        style={{width: 40, height: 40,  marginLeft: -7}}
-                      />
-                    </View>
-                    <Text text40>{isRTL() ? principle.name : principle.nameEn}</Text>
+                    <Text text40 style={{writingDirection: isRTL() ? 'rtl' : 'ltr'}}>{isRTL() ? obj.name : obj.nameEn}</Text>
                   </View>
-                  <Text text70 style={{writingDirection: isRTL() ? 'rtl' : 'ltr'}}>{isRTL() ? principle.description : principle.descriptionEn}</Text>
+                  <Text text70 style={{writingDirection: isRTL() ? 'rtl' : 'ltr'}}>{isRTL() ? obj.description : obj.descriptionEn}</Text>
                 </View>
               );
             })
