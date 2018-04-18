af56e2e7f4323add4901b00e7857626ae741e5b0
yogevbd <yogevbd@wix.com>
2018-04-18 13:35:28 +0300

    added pageControl in art screen

--- src/screens/Art/ArtDetailsScreen.js
+++ src/screens/Art/ArtDetailsScreen.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import {ScrollView, Image, Dimensions, Linking} from 'react-native';
-import {Text, View, Button, Carousel} from 'react-native-ui-lib';
+import {Text, View, Button, Carousel, PageControl, Colors} from 'react-native-ui-lib';
 import {getRandomImage} from '../../../data/img';
 import {EventsComponent} from './../Now/EventsComponent';
 import * as store from '../../stores/campsAndArt/store';
@@ -16,13 +16,28 @@ const IMAGE_SIZE = {
 };
 
 export default class ArtDetailsScreen extends Component {
+  
+  state = {
+    currentPage: 0,
+  };
+
+  onChangePage(index) {
+    this.setState({currentPage: index});
+  }
+
   renderImages() {
     return (
-      <Carousel>
-        {this.props.images.map((image, index) => {
-          return this.renderImagePage(image, index);
-        })}
-      </Carousel>
+      <View>
+        <Carousel onChangePage={(index => this.onChangePage(index))}>
+          {this.props.images.map((image, index) => {
+            return this.renderImagePage(image, index);
+          })}
+        </Carousel>
+        <PageControl
+          numOfPages={this.props.images.length} currentPage={this.state.currentPage} inactiveColor={Colors.dark70} color={Colors.dark30}
+          enlargeActive containerStyle={{bottom: 15}}
+        />
+      </View>
     );
   }
 
