ac51ae071239eb3bfaa8a6be6e15bd8c7f6f07df
Ran Greenberg <rang@wix.com>
2018-04-21 22:20:38 +0300

    update android app icons and app name

--- android/app/src/main/java/com/midburnmobileapp/MainActivity.java
+++ android/app/src/main/java/com/midburnmobileapp/MainActivity.java
@@ -1,20 +1,8 @@
 package com.midburnmobileapp;
 import org.devio.rn.splashscreen.SplashScreen;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-//public class MainActivity extends ReactActivity {
-//
-//    /**
-//     * Returns the name of the main component registered from JavaScript.
-//     * This is used to schedule rendering of the component.
-//     */
-//    @Override
-//    protected String getMainComponentName() {
-//        return "MidburnMobileApp";
-//    }
-//}
 
 
 import com.reactnativenavigation.controllers.SplashActivity;
