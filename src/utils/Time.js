62393959eb998750c3809eee2ebe7e815d76501c
Ran Greenberg <rang@wix.com>
2018-04-27 23:46:46 +0300

    add date to time presented

--- src/utils/Time.js
+++ src/utils/Time.js
@@ -1,5 +1,7 @@
 import moment from 'moment';
 
 export function getHourForTime(time) {
-  return moment(time, 'X').format('HH:mm');
+  // return moment(time, 'X').format('HH:mm');
+  return moment(time, 'X').format('MM/DD HH:mm');
+  // return moment(time, 'X').fromNow();
 }
