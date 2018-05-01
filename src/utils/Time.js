2fdeaa9906c2d31e3c7f8cfdaf9358676f28583d
Ran Greenberg <rang@wix.com>
2018-05-01 20:35:03 +0300

    fix date format on event component

--- src/utils/Time.js
+++ src/utils/Time.js
@@ -2,6 +2,5 @@ import moment from 'moment';
 
 export function getHourForTime(time) {
   // return moment(time, 'X').format('HH:mm');
-  return moment(time, 'X').format('MM/DD HH:mm');
-  // return moment(time, 'X').fromNow();
+  return moment(time, 'X').format('DD/MM HH:mm');
 }
