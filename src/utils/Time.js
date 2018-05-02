cde4975deeaa17ecc00fe0fa88fc754b8d131998
Ran Greenberg <rang@wix.com>
2018-05-02 09:40:25 +0300

    get day hour for time

--- src/utils/Time.js
+++ src/utils/Time.js
@@ -2,5 +2,13 @@ import moment from 'moment';
 
 export function getHourForTime(time) {
   // return moment(time, 'X').format('HH:mm');
-  return moment(time, 'X').format('DD/MM HH:mm');
+  return getMomentObject(time).format('DD/MM HH:mm');
+}
+
+export function getMomentObject(time) {
+  return moment(time, 'X');
+}
+
+export function getHourFromTime(time) {
+  return getMomentObject(time).hour();
 }
