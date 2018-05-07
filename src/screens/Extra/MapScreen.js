2449ffe1252bcd0d37cbcc6d49c734c30a5e8a86
Noam Almog <noama@wix.com>
2018-05-07 12:46:32 +0300

    added platform specific code for android/ios

--- src/screens/Extra/MapScreen.js
+++ src/screens/Extra/MapScreen.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import { StyleSheet, Dimensions, View } from 'react-native';
+import { StyleSheet, Dimensions, View, Platform } from 'react-native';
 
 import Pdf from 'react-native-pdf';
 
 export default class MapScreen extends React.Component {
     render() {
-        // const source = {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf',cache:true};
-        const source = require('../../../data/2018/map/CityMap2018.pdf');  // ios only
-        //const source = {uri:'bundle-assets://test.pdf'};
-
-        //const source = {uri:'file:///sdcard/test.pdf'};
-        //const source = {uri:"data:application/pdf;base64,..."};
+        const source = Platform.select({
+            ios: () => require('../../../data/2018/map/CityMap2018.pdf'),  // ios only
+            android: () => { uri:'bundle-assets://test.pdf' }, // change to real path, or add to android asserts
+        })();
 
         return (
             <View style={styles.container}>
                 <Pdf
                     source={source}
-                    onLoadComplete={(numberOfPages,filePath)=>{
-                        console.log(`number of pages: ${numberOfPages}`);
-                    }}
-                    onPageChanged={(page,numberOfPages)=>{
-                        console.log(`current page: ${page}`);
-                    }}
-                    onError={(error)=>{
-                        console.log(error);
-                    }}
                     style={styles.pdf}/>
             </View>
         )
