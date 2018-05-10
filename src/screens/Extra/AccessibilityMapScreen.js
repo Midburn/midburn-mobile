386a181fb66e50326cae855b95d2fdd64edd3c72
Ran Greenberg <rang@wix.com>
2018-05-11 02:01:30 +0300

    fix maps on android

--- src/screens/Extra/AccessibilityMapScreen.js
+++ src/screens/Extra/AccessibilityMapScreen.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Dimensions, BackHandler} from 'react-native';
+import {Dimensions, BackHandler, Platform} from 'react-native';
 import {View} from 'react-native-ui-lib'
 import Pdf from 'react-native-pdf';
 
-const AccessibilityMap = require('../../../data/2018/map/CityMap2018Accessibility.pdf');
+const ACCESSIBILITY_MAP = require('../../../data/2018/map/CityMap2018Accessibility.pdf');
+const IS_IOS = Platform.OS === 'ios';
 
 
 export default class MapScreen extends React.Component {
   constructor(props) {
     super(props);
-
+    this.source = IS_IOS ? ACCESSIBILITY_MAP : {uri: 'bundle-assets://data/2018/map/CityMap2018Accessibility.pdf'}
   }
 
   componentDidMount() {
@@ -25,7 +26,7 @@ export default class MapScreen extends React.Component {
     return (
       <View flex center>
         <Pdf
-          source={AccessibilityMap}
+          source={this.source}
           style={{flex: 1, width: Dimensions.get('window').width}}
         />
       </View>
--- src/screens/Extra/MapScreen.js
+++ src/screens/Extra/MapScreen.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Dimensions, BackHandler} from 'react-native';
+import {Dimensions, BackHandler, Platform} from 'react-native';
 import {View} from 'react-native-ui-lib'
 import Pdf from 'react-native-pdf';
 
 const MAP = require('../../../data/2018/map/CityMap2018.pdf');
-
+const IS_IOS = Platform.OS === 'ios';
 
 export default class MapScreen extends React.Component {
   constructor(props) {
     super(props);
+    this.source = IS_IOS ? MAP : {uri: 'bundle-assets://data/2018/map/CityMap2018.pdf'}
 
   }
 
@@ -25,7 +26,7 @@ export default class MapScreen extends React.Component {
     return (
       <View flex center>
         <Pdf
-          source={MAP}
+          source={this.source}
           style={{flex: 1, width: Dimensions.get('window').width}}
         />
       </View>
